fe4d1fd636ad8916aef58bd45d519bc162b2a80f
gregoire.eloken <gregoire@eloken.com>
2023-01-03 22:34:32 +0100

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className=" h-screen">
+
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="description" content="Grillz sur-mesure (bijoux dentaires Sur Mesure & amovibles) livré partout en France ! 🇫🇷"/>
+            <title>Your Grillz</title>
+        </Head>
+
       <body>
         <Main />
         <NextScript />
